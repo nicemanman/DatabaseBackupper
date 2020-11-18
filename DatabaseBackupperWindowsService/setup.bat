3a307ce7bb7645b802da5ef43497b330d381d73f
i.falko <i.falko@ecmgroup.pro>
2020-11-18 13:07:20 +0300

    Правки в файле setup.bat

--- DatabaseBackupperWindowsService/setup.bat
+++ DatabaseBackupperWindowsService/setup.bat
@@ -1,3 +1,30 @@
-net stop DatabaseBackupperService
-C:\Windows\Microsoft.NET\Framework64\v4.0.30319\InstallUtil.exe .\bin\Debug\DatabaseBackupperWindowsService.exe
-net start DatabaseBackupperService
+@echo off
+
+set "ServiceName=DatabaseBackupperService"
+set "ServiceDisplayName=DatabaseBackupperService"
+set "ServiceDescription=Background services host for database backupper app"
+
+
+
+set "CurrentDir=%~dp0"
+if "%CurrentDir:~-1%" == "\" (
+    set "CurrentDir=%CurrentDir:~0,-1%"
+)
+pushd "%CurrentDir%"
+
+echo  Please, make sure you run the script as Administrator
+echo;
+
+echo  ^> Installing service "%ServiceName%" with Name = "%ServiceDisplayName%"
+echo;
+
+rem https://support.microsoft.com/en-us/help/251192/how-to-create-a-windows-service-by-using-sc-exe
+
+sc create "%ServiceName%" DisplayName= "%ServiceDisplayName%" start= auto binpath= "%CurrentDir%\Chronos.exe --service"
+
+sc description "%ServiceName%" "%ServiceDescription%"
+if not "%ErrorLevel%"=="0" goto :Finish
+
+:Finish
+echo;
+pause
