707cbfc0560c70ebeeb0a1554ed115c2afd55ba6
nicemanman\Илья <ilya.falko2013@gmail.com>
2021-01-29 09:06:00 +0300

    Добавил либы Google.Apis.Auth, Google.Apis.Drive

--- UI/TaskDetail.Designer.cs
+++ UI/TaskDetail.Designer.cs
@@ -88,7 +88,7 @@
             // button2
             // 
             this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.button2.Location = new System.Drawing.Point(353, 344);
+            this.button2.Location = new System.Drawing.Point(353, 343);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(130, 23);
             this.button2.TabIndex = 9;
@@ -168,7 +168,7 @@
             // 
             this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.Location = new System.Drawing.Point(457, 256);
+            this.button1.Location = new System.Drawing.Point(457, 255);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(26, 23);
             this.button1.TabIndex = 33;
