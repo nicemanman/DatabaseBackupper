bcef44083922049ebe6ae34944107e2d5454bd45
nicemanman\Илья <ilya.falko2013@gmail.com>
2021-03-01 17:15:15 +0300

    Добавил глупый тест для настройки travis ci

---
+++ Presentation.Tests/LoginPresenterTests.cs
@@ -0,0 +1,36 @@
+﻿using DomainModel.Services;
+using NSubstitute;
+using NUnit.Framework;
+using Presentation.Common;
+using Presentation.Presenters;
+using Presentation.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentation.Tests
+{
+    [TestFixture]
+    public class LoginPresenterTests
+    {
+        private ILoginView _view;
+        private IApplicationController _controller;
+        [SetUp]
+        public void SetUp()
+        {
+            _controller = Substitute.For<IApplicationController>();
+            _view = Substitute.For<ILoginView>();
+            var service = Substitute.For<ILoginService>();
+            var presenter = new LoginPresenter(_controller, _view, service);
+            presenter.Run();
+        }
+
+        [Test]
+        public void Test() 
+        {
+            Assert.IsTrue(1 == 1);
+        }
+    }
+}
