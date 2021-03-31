5bb3e2043a31d6ceaa659e209e60201e4fe236f8
nicemanman\Илья <ilya.falko2013@gmail.com>
2021-03-31 05:26:11 +0300

    Черновик приложения, отвечающего за иконку сервера бэкапа в трее

---
+++ BackupServerTray/MyAppContext.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows.Forms;
+
+namespace BackupServerTray
+{
+    internal class MyAppContext : ApplicationContext
+    {
+        private NotifyIcon _notifyIcon;
+        public MyAppContext()
+        {
+            _notifyIcon = new NotifyIcon()
+            {
+                ContextMenu = new ContextMenu(new[] 
+                {
+                    new MenuItem() 
+                    {
+                        Text = "Выйти"
+                    } 
+                }),
+                Visible = true
+            };
+        }
+    }
+}
